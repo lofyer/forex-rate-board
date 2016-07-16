ba922b1c88fd507f2aa9a370a9ecdf128f3748a9
lofyer <lofyer@gmail.com>
2016-07-17 01:24:28 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
 import matplotlib
+import time
 from yahoo_finance import Currency,Share
 
-cur_list = {"EURUSD":"", "EURGBP":"", "USDGBP":""}
+def get_data(symbol):
+    return Currency(symbol).get_rate()
 
-for cur in cur_list.keys():
-    cur_list[cur] = Currency(cur).get_rate()
+cur_list = ["EURUSD", "EURGBP", "USDGBP"]
+
 
 print cur_list
+while True:
+    cur_list_data = map(get_data, cur_list)
+    print cur_list_data
+    time.sleep(5)
