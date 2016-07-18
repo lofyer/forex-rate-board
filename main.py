e185ea33d3eb83ce1d42d8e3b7876d9fba305c2f
lofyer <lofyer@gmail.com>
2016-07-18 10:40:51 +0800

    Update

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #!/usr/local/bin/python
 import matplotlib
 import time
+from datetime import datetime
 from yahoo_finance import Currency,Share
+
+cur_list = ["EURUSD", "EURGBP", "USDGBP" ]
+cur_list_str = '\t'.join(str(x) for x in cur_list)
+
+TITLE = "DATE\t\t\t%s" % cur_list_str
+BODY = ""
 
 def get_data(symbol):
     return Currency(symbol).get_rate()
 
-cur_list = ["EURUSD", "EURGBP", "USDGBP"]
+print TITLE
 
-
-print cur_list
 while True:
-    cur_list_data = map(get_data, cur_list)
-    print cur_list_data
+    cur_list_data_str = '\t'.join(str(x) for x in map(get_data, cur_list))
+    print "%s\t%s" % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), cur_list_data_str)
     time.sleep(5)
